583ec31512e508b529f5c0716ba8d75d70053100
Jorge Chamorro Bieling <jorge@jorgechamorro.com>
2011-11-03 20:47:08 +0100

    añadir test sinewave(s).js

---
+++ test-sinewave.js
@@ -0,0 +1,54 @@
+//2011-11-03 Jorge@jorgechamorro.com
+//Cómo sintetizar una onda sinusoidal
+
+
+function createSineWave (f /*frequency in Hz*/, seconds /*duration in seconds*/) {
+  //returns a buffer containing a sound of a sine wave of frequency f and duration seconds in PCM format
+
+  function floatToSignedInteger (value) {
+    //converts from float value in the range -1..1 to a 16bits signed integer
+
+    function lonibble (integer) {
+      return integer & 0x00ff;
+    }
+
+    function hinibble (integer) {
+      return (integer & 0xff00) >>> 8;
+    }
+
+    if (value > 1) value= 1;
+    else if (value < -1) value= -1;
+    value= Math.floor(32767*value);
+    return {hi:hinibble(value), lo:lonibble(value), v:value};
+  }
+  
+  var kChannels= 2;
+  var kBytesPerSample= 2;
+  var kSamplesPerSecond= 44100;
+  var buffer= new Buffer(Math.floor(seconds*kSamplesPerSecond)* kChannels* kBytesPerSample);
+
+  var i= 0;
+  var step= kChannels* kBytesPerSample;
+  do {
+    var α= f* 2* Math.PI* i/ kSamplesPerSecond/ step;
+    var sample= floatToSignedInteger(Math.sin(α));
+    //process.stdout.write([i/step, α, sample.v, sample.hi, sample.lo] + "\r\n");
+    buffer[i]= buffer[i+2]= sample.lo;
+    buffer[i+1]= buffer[i+3]= sample.hi;
+    i+= step;
+  } while (i < buffer.length);
+
+  return buffer;
+}
+
+
+var DONE= 0;
+var Sound= require('./build/default/sound');
+var buffer= createSineWave(1e3/*f in Hz*/, 1/*duration in seconds*/);
+var sound= Sound.create(buffer);
+sound.loop(3).play(cb);
+function cb () {
+  DONE= 1;
+}
+
+(function cb () { if (!DONE) setTimeout(cb, 333); })();
---
+++ test-sinewaves.js
@@ -0,0 +1,62 @@
+//2011-11-03 Jorge@jorgechamorro.com
+//Hace una musiquilla sintetizando unas cuantas sinusoidales randommente
+
+
+function createSineWave (f /*frequency in Hz*/, seconds /*duration in seconds*/) {
+  //returns a buffer containing a sound of a sine wave of frequency f and duration seconds in PCM format
+
+  function floatToSignedInteger (value) {
+    //converts from float value in the range -1..1 to a 16bits signed integer
+
+    function lonibble (integer) {
+      return integer & 0x00ff;
+    }
+
+    function hinibble (integer) {
+      return (integer & 0xff00) >>> 8;
+    }
+
+    if (value > 1) value= 1;
+    else if (value < -1) value= -1;
+    value= Math.floor(32767*value);
+    return {hi:hinibble(value), lo:lonibble(value), v:value};
+  }
+  
+  var kChannels= 2;
+  var kBytesPerSample= 2;
+  var kSamplesPerSecond= 44100;
+  var buffer= new Buffer(Math.floor(seconds*kSamplesPerSecond)* kChannels* kBytesPerSample);
+
+  var i= 0;
+  var step= kChannels* kBytesPerSample;
+  do {
+    var α= f* 2* Math.PI* i/ kSamplesPerSecond/ step;
+    var sample= floatToSignedInteger(Math.sin(α));
+    //process.stdout.write([i/step, α, sample.v, sample.hi, sample.lo] + "\r\n");
+    buffer[i]= buffer[i+2]= sample.lo;
+    buffer[i+1]= buffer[i+3]= sample.hi;
+    i+= step;
+  } while (i < buffer.length);
+
+  return buffer;
+}
+
+
+var DONE= 0;
+var Sound= require('./build/default/sound');
+
+var musiquilla= [];
+var i= 10;
+while (i--) {
+  var f= 441+ (1e3* Math.random());
+  var t= 0.1+ (Math.random()/5);
+  var buffer= createSineWave(f/*f in Hz*/, t/*duration in seconds*/);
+  musiquilla.push(Sound.create(buffer));
+}
+
+(function cb () {
+  if (!musiquilla.length) return (DONE= 1);
+  musiquilla.pop().play(cb);
+})();
+
+(function cb () { if (!DONE) setTimeout(cb, 333); })();
